280fb6feba375e3311c6b24f0f9cbfa969050f57
Isidoro Ghezzi <isidoroghezzi@salgroup.it>
2021-05-27 10:55:27 +0200

    remove 'git add -p .'

--- gwap.sh
+++ gwap.sh
@@ -1,3 +1,3 @@
 # from https://stackoverflow.com/questions/6571643/how-can-you-combine-git-add-patch-p-mode-with-diffs-ignore-all-space
-alias gwap='git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero && git checkout -- . && git reset && git add -p'
+alias gwap='git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero && git checkout -- . && git reset'
 
