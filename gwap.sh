aa9b4a49affb17661cbf23c47c575f488d6e3b2f
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2022-04-07 11:07:51 +0200

    more specific stackoverflow link

--- gwap.sh
+++ gwap.sh
@@ -1,3 +1,3 @@
-# from https://stackoverflow.com/questions/6571643/how-can-you-combine-git-add-patch-p-mode-with-diffs-ignore-all-space
+# from [how-can-you-combine-git-add-patch-p-mode-with-diffs-ignore-all-space](https://stackoverflow.com/a/44785950)
 alias gwap='git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero && git checkout -- . && git reset'
 
